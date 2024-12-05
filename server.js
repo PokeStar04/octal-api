a73c343707b758c102613933ff55a2b12983fc9c
Romain <pokestar04@gmail.com>
2024-12-05 12:07:16 +0100

    add user id

--- server.js
+++ server.js
@@ -618,6 +618,7 @@ app.get('/create_users-combined-data-in-db', async (req, res) => {
                     const insertResponse = await supabase.from('userVerified').insert([
                         {
                             id: user.id,
+                            userId: user.userId,
                             nom: user.nom,
                             prenom: user.prenom,
                             numero: user.numero,
